bd1afade76b17e53dc8486e9b061e7d73a3aaf59
Nikol_Niven <ntopalusic@gmail.com>
2025-03-12 16:34:59 +0100

    feat: add type aliases and interfaces for media items; implement function to calculate years since release

--- src/iteration4.ts
+++ src/iteration4.ts
@@ -1,5 +1,40 @@
-//
-// Iteration 4 | Type aliases
-//
+type Book = {
+  title: string;
+  year: number;
+  genres: string[];
+  rating?: number;
+  author: string;
+  numberOfPages?: number;
+};
 
+type Movie = {
+  title: string;
+  year: number;
+  genres: string[];
+  rating?: number;
+  director: string;
+  durationInMinutes?: number;
+};
 
+enum AlbumType {
+  StudioAlbum = 'Studio Album',
+  LiveAlbum = 'Live Album',
+  Soundtrack = 'Soundtrack',
+  Other = 'Other',
+}
+
+type MusicAlbum = {
+  title: string;
+  year: number;
+  genres: string[];
+  rating?: number;
+  artist: string;
+  numberOfTracks?: number;
+  durationInMinutes?: number;
+  albumType: AlbumType;
+};
+
+function getYearsSinceRelease(object: Book | Movie | MusicAlbum): number {
+  const currentYear: number = new Date().getFullYear();
+  return currentYear - object.year;
+}
--- src/iteration5.ts
+++ src/iteration5.ts
@@ -1,5 +1,36 @@
-//
-// Bonus: Iteration 5 | Interfaces
-//
+interface MediaItem {
+  title: string;
+  year: number;
+  genres: string[];
+  rating?: number;
+}
 
+interface Book extends MediaItem {
+  author: string;
+  numberOfPages?: number;
+}
 
+interface Movie extends MediaItem {
+  director: string;
+  durationInMinutes?: number;
+}
+
+enum AlbumType {
+  StudioAlbum = 'Studio Album',
+  LiveAlbum = 'Live Album',
+  Soundtrack = 'Soundtrack',
+  Other = 'Other',
+}
+
+interface MusicAlbum extends MediaItem {
+  artist: string;
+  numberofTracks?: number;
+  durationInMinutes?: number;
+  albumType: AlbumType;
+}
+
+function getYearsSinceRelease(mediaItem: MediaItem): number {
+  const currentYear = new Date().getFullYear();
+  const difference = currentYear - mediaItem.year;
+  return difference;
+}
