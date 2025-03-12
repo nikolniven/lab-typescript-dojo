6dc38940e306bfdead5cc957b90fbed76b7e8b08
Nikol_Niven <ntopalusic@gmail.com>
2025-03-12 14:44:45 +0100

    feat: implement functions for greeting, multiplication, even check, and array average; add BankAccount class with deposit, withdraw, and balance methods

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -1,5 +1,18 @@
-//
-// Iteration 2 | Functions
-//
+function calcMultiplication(a: number, b:number): number {
+  return a * b;
+}
 
+function isEven(n: number): boolean {
+  return  n % 2 === 0  ;
+}
+
+function calcArrayAverage(param: number[]): string{
+    if (param.length === 0 )
+    return "Please provide an array with at least one element";
+else{
+    let sum = 0;
+    param.forEach(num => sum += num )
+    return (sum/ param.length).toString()
+}
+} 
 
--- src/iteration3.ts
+++ src/iteration3.ts
@@ -1,5 +1,32 @@
-//
-// Iteration 3 | Classes
-//
+class BankAccount {
+  balance: number = 0;
+  accountHolder: string;
 
+  constructor(accountHolder: string) {
+    this.accountHolder = accountHolder;
+  }
 
+  deposit(amount: number): number | string {
+    if (amount <= 0) {
+      return 'Please provide a valid amount';
+    }
+    this.balance += amount;
+    return this.balance;
+  }
+
+  withdraw(amount: number): number | string {
+    if (amount <= 0) {
+      return 'Please provide a valid amount';
+    }
+    if (amount > this.balance) {
+      return 'Insufficient funds';
+    }
+    this.balance -= amount;
+    return this.balance;
+  }
+  getBalance() {
+    return this.balance;
+  }
+}
+
+new BankAccount('Cristo Dio');
